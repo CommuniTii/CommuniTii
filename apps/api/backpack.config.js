8ce2fb1ecb82c1a0290430ff224f09f824e855f4
Walee Ahmed <dev@walee.me>
2018-11-10 15:02:14 -0500

    Modular schema/resolver setup through .graphql import support

--- apps/api/backpack.config.js
+++ apps/api/backpack.config.js
@@ -1,10 +1,18 @@
+const graphqlLoaderRule = {
+  test: /\.(graphql|gql)$/,
+  exclude: /node_modules/,
+  loader: "graphql-tag/loader"
+}
+
 module.exports = {
   webpack: (config, options, webpack) => {
     // Perform customizations to config
     // Important: return the modified config
 
-    // changes the name of the entry point from index -> main.js
+    // changes the name of the entry point to index.js in the project root
     config.entry.main = ["./index.js"]
+    // load .graphql files
+    config.module.rules.push(graphqlLoaderRule)
 
     return config
   }
