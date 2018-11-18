e8aebb695d9bfb7ef7528e56cb3d2e9b141bbdaf
Walee Ahmed <dev@walee.me>
2018-11-18 01:30:47 -0500

    cleaner initial bootstrapping

--- apps/api/index.js
+++ apps/api/index.js
@@ -1,35 +1,17 @@
-import express from "express"
-import { ApolloServer } from "apollo-server-express"
-
-import getContext from "./lib/getContext"
-import getSchema from "./lib/getSchema"
-import initMongo from "./lib/initMongo"
-import getModels from "./models"
+import initBootstrap from "./lib/initBootstrap"
 
 const initApp = async () => {
-  const mongoUrl = `mongodb://${process.env.DB_USERNAME}:${
-    process.env.DB_PASSWORD
-  }@${process.env.DB_URL}`
-
-  const mongo = await initMongo(mongoUrl)
-  const models = getModels(mongo)
-  const schema = getSchema()
-
-  const server = new ApolloServer({
-    schema,
-    context: async req => getContext(req, models)
-  })
-
-  const app = express()
-  server.applyMiddleware({ app })
+  const { app, server } = await initBootstrap()
 
   app.listen({ port: 4000 }, () =>
-    console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
+    console.log(
+      `🚀  Server ready at http://localhost:4000${server.graphqlPath}`
+    )
   )
 }
 
 try {
   initApp()
 } catch (e) {
-  console.error(e.message, "🔥")
+  console.error("🔥 ", e.message)
 }
---
+++ apps/api/lib/initBootstrap.js
@@ -0,0 +1,27 @@
+import express from "express"
+import { ApolloServer } from "apollo-server-express"
+
+import getContext from "./getContext"
+import getSchema from "./getSchema"
+import initMongo from "./initMongo"
+import getModels from "../models"
+
+export default async () => {
+  const mongoUrl = `mongodb://${process.env.DB_USERNAME}:${
+    process.env.DB_PASSWORD
+  }@${process.env.DB_URL}`
+
+  const mongo = await initMongo(mongoUrl)
+  const models = getModels(mongo)
+  const schema = getSchema()
+
+  const server = new ApolloServer({
+    schema,
+    context: async req => getContext(req, models)
+  })
+
+  const app = express()
+  server.applyMiddleware({ app })
+
+  return { app, server }
+}
