12b436c7d89f8b99300511b1319b91e10afe4c02
John Settineri <settineri.john@gmail.com>
2018-11-21 23:45:48 -0500

    Adding username check

--- apps/api/models/Users.js
+++ apps/api/models/Users.js
@@ -2,7 +2,6 @@ import { hash, compare } from "bcrypt"
 import { sign } from "jwt-then"
 import { AuthenticationError } from "apollo-server"
 import { ObjectId } from "mongodb"
-
 export default ({ users }) => {
   const findUserByUserName = async userName => users.findOne({ userName })
 
@@ -12,6 +11,7 @@ export default ({ users }) => {
 
   const addUser = async userInfo => {
     // TODO: Add checks on user info and return appropriate errors
+    if(userInfo.password)
     return users.insertOne(userInfo)
   }
 
@@ -19,9 +19,15 @@ export default ({ users }) => {
     // TODO: Add check to see if userName & email exists already
     const user = { ...userInfo, password: await hash(password, 10) }
     await addUser(user)
-
-    const { _id: id, userName, email } = user
-    return sign({ id, userName, email }, process.env.JWT_SECRET)
+    const user = await findUserByUserName(userInfo.userName)
+    if(!user)
+    {
+      const { _id: id, userName, email } = user
+      return sign({ id, userName, email }, process.env.JWT_SECRET)
+    }
+    else {
+      throw Error("Username already in use");
+    }
   }
 
   const signIn = async ({ userName, password }) => {
