93c020cef6f2ca92a6ce23ac1d05fb33af25d5c2
Walee Ahmed <dev@walee.me>
2018-12-08 14:54:13 -0500

    centered info

---
+++ apps/landing/lib/GlobalStyle.js
@@ -0,0 +1,12 @@
+import { createGlobalStyle } from "styled-components"
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    height: 100vh;
+  }
+
+  #__next {
+    height: 100%
+  }
+`
+export default GlobalStyle
--- apps/landing/pages/index.js
+++ apps/landing/pages/index.js
@@ -1,19 +1,35 @@
 import React from "react"
-import { Box, Text, Image } from "rebass"
+import { Flex, Box, Text, Image } from "rebass"
+import styled from "styled-components"
 
+import GlobalStyle from "../lib/GlobalStyle"
+
+const Container = styled.div`
+  height: 100%;
+`
 const LandingPage = () => (
-  <Box>
-    <Image width={[1 / 3, 1 / 4]} src="/static/logo.png" borderRadius={8} />
-    <Text fontSize={[6, 7, 8]} fontWeight="bold">
-      CommuniTii
-    </Text>
-    <Text fontSize={[2, 3, 4]}>The Game of Life is Better Together</Text>
-    <Text fontSize={[3, 4, 5]}>Coming Soon...</Text>
+  <>
+    <GlobalStyle />>
+    <Container>
+      <Flex
+        py={[1, 3, 5]}
+        alignItems="center"
+        justifyContent="center"
+        flexDirection="column"
+      >
+        <Image width={[1 / 3, 1 / 4]} src="/static/logo.png" borderRadius={8} />
+        <Text fontSize={[6, 7, 8]} fontWeight="bold">
+          CommuniTii
+        </Text>
+        <Text fontSize={[2, 3, 4]}>The Game of Life is Better Together</Text>
+        <Text fontSize={[3, 4, 5]}>Coming Soon...</Text>
 
-    <Text fontSize={[1]}>
-      Got a question? Lets chat hello@communitiiapp.com
-    </Text>
-  </Box>
+        <Text fontSize={[1]}>
+          Got question? Email us hello@communitiiapp.com
+        </Text>
+      </Flex>
+    </Container>
+  </>
 )
 
 export default LandingPage
