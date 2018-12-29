aa6cbcba5e1b87ea9eb3aeb36099aa6218076bf7
Walee Ahmed <dev@walee.me>
2018-12-28 19:57:17 -0500

    added material ui support w/ @Dragonia428

--- apps/landing/pages/index.js
+++ apps/landing/pages/index.js
@@ -1,34 +1,29 @@
 import React from "react"
 import { Flex, Text, Image } from "rebass"
-import styled from "styled-components"
 
-import GlobalStyle from "../lib/GlobalStyle"
-
-const Container = styled.div`
-  height: 100%;
-`
-const LandingPage = () => (
+const LandingPage = ({ classes }) => (
   <>
-    <GlobalStyle />
-    <Container>
-      <Flex
-        py={[1, 3, 5]}
-        alignItems="center"
-        justifyContent="center"
-        flexDirection="column"
-      >
-        <Image width={[1 / 3, 1 / 4]} src="/static/logo.png" borderRadius={8} />
-        <Text fontSize={[6, 7, 8]} fontWeight="bold">
-          CommuniTii
-        </Text>
-        <Text fontSize={[2, 3, 4]}>The Game of Life is Better Together</Text>
-        <Text fontSize={[3, 4, 5]}>Coming Soon...</Text>
+    <Flex
+      style={{ height: "100vh" }}
+      py={[1, 3, 5]}
+      alignItems="center"
+      flexDirection="column"
+    >
+      <Image
+        width={[2 / 3, 2 / 3, 1 / 3, 1 / 7]}
+        src="/static/logo.png"
+        borderRadius={8}
+      />
+      <Text fontSize={[6, 7]} fontWeight="bold">
+        CommuniTii
+      </Text>
+      <Text fontSize={[4, 3]}>The Game of Life is Better Together</Text>
+      <Text fontSize={[5, 3]}>Coming Soon...</Text>
 
-        <Text fontSize={[1]}>
-          Got questions? Email us hello@communitiiapp.com
-        </Text>
-      </Flex>
-    </Container>
+      <Text fontSize={[3]} style={{ paddingTop: "40vh" }}>
+        Got questions? Email us hello@communitiiapp.com
+      </Text>
+    </Flex>
   </>
 )
 
