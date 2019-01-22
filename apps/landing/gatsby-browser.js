78fbfc9fb7c959277fad31a5ca9bfcbe2e008489
Walee Ahmed <dev@walee.me>
2019-01-23 00:20:04 +0100

    fixed ssr and added support w/ material ui with plugin

--- apps/landing/gatsby-browser.js
+++
@@ -1,8 +0,0 @@
-import React from "react"
-import withRoot from "./src/lib/withRoot"
-
-const WithRoot = withRoot(props => props.children)
-
-export const wrapRootElement = ({ element }) => {
-  return <WithRoot key={Math.random()}>{element}</WithRoot>
-}
